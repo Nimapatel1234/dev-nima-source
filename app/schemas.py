70ef02e9d18c10ca4d640e295da3688359f9ac89
Nima Patel <nima2001patel@gmail.com>
2025-01-22 13:40:31 +0530

    dev_13:45

--- app/schemas.py
+++ app/schemas.py
@@ -23,6 +23,40 @@
 #         orm_mode = True
 
 
+
+############################
+
+
+
+# from pydantic import BaseModel
+# from typing import List, Optional
+
+# # Author schema
+# class BooksAuthorBase(BaseModel):
+#     id: int
+#     name: str
+#     birth_year: Optional[int] = None
+#     death_year: Optional[int] = None
+
+#     class Config:
+#         orm_mode = True
+
+# # Book schema
+# class BooksBookBase(BaseModel):
+#     id: int
+#     title: str
+#     media_type: str
+#     download_count: Optional[int] = None
+#     gutenberg_id: int
+#     authors: List[BooksAuthorBase] = []
+
+#     class Config:
+#         orm_mode = True
+
+
+###################################
+
+
 from pydantic import BaseModel
 from typing import List, Optional
 
@@ -43,7 +77,7 @@ class BooksBookBase(BaseModel):
     media_type: str
     download_count: Optional[int] = None
     gutenberg_id: int
-    authors: List[BooksAuthorBase] = []
+    authors: List[BooksAuthorBase] = []  # Authors data included here
 
     class Config:
         orm_mode = True
